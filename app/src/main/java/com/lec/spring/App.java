b1b1c60a35215ac6aa966ad93031a4886026d798
jung-yoon-sung <mrj951230@naver.com>
2024-11-16 16:48:10 +0900

    main directory 수정

--- app/src/main/java/com/lec/spring/App.java
+++ app/src/main/java/com/lec/spring/App.java
@@ -1,10 +1,11 @@
-package demo;
+package com.lec.spring;
 
-import com.gradle.CustomApp;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
     public String getGreeting() {
-        return "CustomApp identifier is: " + CustomApp.identifier;
+        return "CustomApp identifier is test";
     }
 
     public static void main(String[] args) {
