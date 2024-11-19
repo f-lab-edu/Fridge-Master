96289b8ce643f917b3a6c3ef6c150eccaa627044
jung-yoon-sung <mrj951230@naver.com>
2024-11-19 10:49:32 +0900

    ingredients 타입 변경

--- app/src/main/java/com/lec/spring/entity/Recipe.java
+++ app/src/main/java/com/lec/spring/entity/Recipe.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,9 +14,9 @@ import lombok.NoArgsConstructor;
 public class Recipe {
     private Long id;
     private String name;
-    private String description;
-    private String[] ingredient;
+    private String steps;
+    private Map<Ingredient, Integer> ingredients;
     private String imageUrl;
-    private String uploadBy;
-    private String uploadOn;
+    private String uploadBy;    // 작성자
+    private String uploadOn;    // 작성일
 }
