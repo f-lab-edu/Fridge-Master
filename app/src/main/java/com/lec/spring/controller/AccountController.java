aeae4c4025df7817211172a11cd83fde6bc90039
jung-yoon-sung <mrj951230@naver.com>
2024-11-19 22:50:24 +0900

    AccountController 수정

--- app/src/main/java/com/lec/spring/controller/AccountController.java
+++ app/src/main/java/com/lec/spring/controller/AccountController.java
@@ -2,32 +2,39 @@ package com.lec.spring.controller;
 
 
 import com.lec.spring.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
 
 
+    public static Long userId = 0L;
 
     @PostMapping("/joinUser")
     public ResponseEntity<?> joinUser(@RequestBody User user) {
 
-        
-        return ResponseEntity.ok("회원가입 성공.");
+        ++userId;
+        user.setId(userId);
+        return ResponseEntity.ok(user);
     }
 
-    @GetMapping("/account/info/{id}")
-    public User accountInfo(@PathVariable Long id) {
-        return null;
+    @GetMapping("/info/{id}")
+    public ResponseEntity<?> accountInfo(@PathVariable Long id) {
+
+        User user = new User();
+
+        return ResponseEntity.ok(user);
     }
 
-    @PutMapping("/account/update/{id}")
-    public String updateUserinfo(@PathVariable Long id, @RequestBody User user) {
-        return "회원정보 수정 완료.";
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUserinfo(@PathVariable Long id, @RequestBody User user) {
+        return ResponseEntity.ok(user);
     }
 
-    @DeleteMapping("/account/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable Long id) {
         return "회원탈퇴 성공.";
     }
