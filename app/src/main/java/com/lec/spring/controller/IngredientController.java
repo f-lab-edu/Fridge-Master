dd9cb51f998736860da5c9029c8d3722774bad4a
jung-yoon-sung <mrj951230@naver.com>
2024-11-20 23:40:48 +0900

    IngredientController 예외 처리

--- app/src/main/java/com/lec/spring/controller/IngredientController.java
+++ app/src/main/java/com/lec/spring/controller/IngredientController.java
@@ -1,33 +1,71 @@
 package com.lec.spring.controller;
 
 
+import com.lec.spring.entity.Ingredient;
+import com.lec.spring.entity.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
 public class IngredientController {
 
-    public static class Ingredient {
-
-    }
+    private static final Map<Long, Ingredient> ingredientStore = new HashMap<Long, Ingredient>();
+    public static Long id = 0L;
 
     @PostMapping("/register")
-    public String registerIng() {
-        return "재료 등록 성공";
+    public ResponseEntity<?> registerIng(@RequestBody Ingredient ing) {
+
+        ing.setId(++id);
+
+
+        ing.builder()
+                .id(ing.getId())
+                .name(ing.getName())
+                .build();
+
+        ingredientStore.put(ing.getId(), ing);
+        return ResponseEntity.ok(ing);
     }
 
     @GetMapping("/search/{ingId}")
-    public Ingredient searchIng(@PathVariable Long ingId) {
-        return null;
+    public ResponseEntity<?> searchIng(@PathVariable Long ingId) {
+
+        if (!ingredientStore.containsKey(ingId)) {
+            return ResponseEntity.ok("유효하지 않은 ID입니다.");
+        }
+
+        return ResponseEntity.ok(ingredientStore.get(ingId));
     }
 
     @PutMapping("/update/{ingId}")
-    public Ingredient updateIng(@PathVariable Long ingId) {
-        return null;
+    public ResponseEntity<?> updateIng(@PathVariable Long ingId, @RequestBody Ingredient updatedIng) {
+
+        Ingredient oldIng = ingredientStore.get(ingId);
+
+        if (!ingredientStore.containsKey(ingId)) {
+            return ResponseEntity.ok("유효하지 않은 ID입니다.");
+        } else if (ingredientStore.containsValue(updatedIng.getName())) {
+            return ResponseEntity.ok("이미 등록된 재료입니다.");
+        } else {
+            updatedIng.setId(ingId);
+            ingredientStore.put(ingId, updatedIng);
+        }
+
+
+        return ResponseEntity.ok(ingredientStore.get(ingId));
     }
 
     @DeleteMapping("/delete/{ingId}")
-    public String deleteIng(@PathVariable Long ingId) {
-        return "삭제 완료";
+    public ResponseEntity<?> deleteIng(@PathVariable Long ingId) {
+        if (!ingredientStore.containsKey(ingId)) {
+            return ResponseEntity.ok("유효하지 않은 ID입니다.");
+        }
+
+        ingredientStore.remove(ingId);
+        return ResponseEntity.ok("삭제 완료");
     }
 }
