56dc27fd6a16993d57f35aad468374c615997c7f
jung-yoon-sung <mrj951230@naver.com>
2025-02-10 23:43:50 +0900

    api DB 저장 수정 필요

--- app/src/main/java/com/lec/spring/controller/RecipeApiController.java
+++ app/src/main/java/com/lec/spring/controller/RecipeApiController.java
@@ -1,11 +1,20 @@
 package com.lec.spring.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.lec.spring.repository.RecipeRepository;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @RestController
 @RequestMapping("/api")
@@ -16,12 +25,24 @@ public class RecipeApiController {
     @Value("${recipe.api.key}")
     private String apikey;
 
-    String apiurl = "https://openapi.foodsafetykorea.go.kr/api/" + apikey;
+    String result = "";
 
     @GetMapping("/{startIndex}/{endIndex}")
     public void saveRecipe(@PathVariable int startIndex, @PathVariable int endIndex) {
+        String apiurl = String.format("https://openapi.foodsafetykorea.go.kr/api/%s/COOKRCP01/json/%d/%d", apikey, startIndex, endIndex);
+        try {
+            URL url = new URL(apiurl);
+            BufferedReader br;
+            br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+
+            result = br.readLine();
+
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
     }
-
-
-}
